28e5e4aec56762f80248b32bb13a3c525602f319
Hazim269 <116706644+Musturde0910@users.noreply.github.com>
2023-11-10 22:29:42 +0800

    V1.02

---
+++ Assets/Visitor/Prefab/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Ray moveposition = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(moveposition, out var hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
